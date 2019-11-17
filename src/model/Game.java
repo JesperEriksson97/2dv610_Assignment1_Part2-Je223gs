ce8a99ff9556881338e82d6316b8afb54236bbf3
Jesper Eriksson <je223gs@student.lnu.se>
2019-11-17 18:09:32 +0100

    GREEN

--- src/model/Game.java
+++ src/model/Game.java
@@ -2,4 +2,20 @@ package model;
 
 public class Game {
 
+	
+	public boolean playerWinsSeven(model.Player player_1) {
+		if (player_1.getDiceValues() == 7) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public boolean playerWinsDoubles(model.Player player_1) {
+		if (player_1.getDice1Val() == player_1.getDice2Val()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
--- src/tests/Game_tests.java
+++ src/tests/Game_tests.java
@@ -1,5 +1,7 @@
 package tests;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
